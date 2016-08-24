bd61da6b1e0d18b7876299bcc2c4c225bf56bc64
Yue.Lee <liyueshining@gmail.com>
2016-08-24 10:26:16 +0800

    Update service-swagger.py

--- service-swagger.py
+++ service-swagger.py
@@ -362,6 +362,7 @@ def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
 
+@app.cli.command()
 def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
